a7808299b92e3b84b1866c87afc637017bbd5ef4
keitakawamitsu <69702777+keitakawamitsu@users.noreply.github.com>
2021-01-25 23:12:46 +0900

    test commit
    テスト

---
+++ index.js
@@ -0,0 +1,17 @@
+//変数
+let unko = 'ハロー';
+
+//unko = '川満恵太';
+const bigUnko = '大きいウンコ'
+
+let inoki = ['いち', '22', '33', '44'];
+
+//let index = 0;
+//while (index < inoki.length) {
+//    console.log(inoki[index]);
+//    index++;
+//}
+
+//console.log(bigUnko);
+//console.log(unko);
+//console.log(inoki[1]); 
