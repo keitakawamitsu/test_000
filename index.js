9fa649613598b2903831de004c7851c61fdd9b74
keitakawamitsu <keitakawamitsu0980@gmail.com>
2021-01-31 09:12:59 +0900

    1/31-9:12 画像を切り離す処理を追加。 by川満

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-//変数
-let unko = 'ハロー';
+var ball = document.getElementById("ball");
 
-//unko = '川満恵太';
-const bigUnko = '大きいウンコ'
+ball.style.position = "absolute";//ここは、ポジションをアブソリュートにしてるってことよね？
 
-let inoki = ['いち', '22', '33', '44'];
+ball.ondragstart = (event) => {
+    return false;
+    //ここの処理、よくわからん
+    //恐らく、ドラッグが始まった時、何もしないよ
+    //っ処理なのかもしれないです
+}
 
-//let index = 0;
-//while (index < inoki.length) {
-//    console.log(inoki[index]);
-//    index++;
-//}
 
-//console.log(bigUnko);
-//console.log(unko);
-//console.log(inoki[1]); 
+ball.onmousedown = function (event) {
+    document.addEventListener("mousemove", onMouseMove);
+}
+var onMouseMove = function (event) {
+    let x = event.clientX;//イベントが起きたときのX座標を参照
+    //console.log(x+'X座標を表示');
+    let y = event.clientY;
+    //console.log(y+'Y座標を表示');
+    let width = ball.offsetWidth;
+    let height = ball.offsetHeight;
+
+    //console.log(width + 'オフセット');
+    //console.log(height + 'オフセット');
+
+    ball.style.top = (y - height / 2) + "px";
+    ball.style.left = (x - width / 2) + "px";
+
+    /*
+    ball.style.top = y + "px";//座標をそのまま持ってくるのは無理っぽい
+    ball.style.left = x + "px";
+    */
+
+    ball.onmouseup = function (event) {
+        //app.removeChild(ball);
+        document.removeEventListener("mousemove", onMouseMove);//マウスが離れたとき、画像を離す
+    }
+
+}
+console.log('チェックログ');//デバック用のチェックログ
